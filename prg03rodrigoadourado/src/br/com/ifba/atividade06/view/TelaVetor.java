bc0a26b61bbcbe8e677736c8e3ef469358d10772
Rodrigo Almeida Dourado <almeidarodrigo201@gmail.com>
2025-05-08 13:08:58 -0300

    atualização no nome do pacote

--- prg03rodrigoadourado/src/br/com/ifba/atividade06/view/TelaVetor.java
+++ prg03rodrigoadourado/src/br/com/ifba/atividade06/view/TelaVetor.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
-package br.com.ifba.atividade06;
+package br.com.ifba.atividade06.view;
 
 import java.util.Arrays;
 import javax.swing.DefaultListModel;
